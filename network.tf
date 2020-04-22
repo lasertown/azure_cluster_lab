0167d134fe72ed8d345468de6c3a78ab43a1d09b
Jason Ju <Jason.Ju@microsoft.com>
2020-04-22 09:23:39 +0000

    Added 12 files to /

---
+++ network.tf
@@ -0,0 +1,46 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}_resources"
+  location = var.region
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  resource_group_name =  azurerm_resource_group.example.name
+  location            =  azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  virtual_network_name =  azurerm_virtual_network.example.name
+  resource_group_name  =  azurerm_resource_group.example.name
+  address_prefix       = "10.0.0.0/24"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "ssh" {
+    name                = "ssh"
+    location            = var.region
+    resource_group_name = azurerm_resource_group.example.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.ssh.id
+}
