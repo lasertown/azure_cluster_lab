bdfdb74812b9e517219b2198e033a51c5a1539a6
Jason Ju <Jason.Ju@microsoft.com>
2020-05-03 02:45:22 +0000

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,20 @@ variable "prefix" {
 variable "region" {
   description = "The Azure location where all resources in this example should be created"
 }
+
+# publisher = "SUSE"
+# offer     = "sles-sap-12-sp5"
+# sku       = "gen2"
+# version   = "latest"
+variable "publisher" {
+  description = "Publisher of the image used to create VM"
+}
+variable "offer" {
+  description = "Offer of the image used to create VM"
+}
+variable "sku" {
+  description = "SKU of the image used to create VM"
+}
+variable "Version" {
+  description = "Version of the image used to create VM"
+}
