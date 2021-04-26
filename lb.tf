375beac12e347587743eec1ac97e0718c1a230b2
lasertown <jasonju@gmail.com>
2021-04-26 11:52:00 -0700

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -20,7 +20,6 @@ resource "azurerm_lb" "lb" {
 }
 
 resource "azurerm_lb_backend_address_pool" "lb" {
-  resource_group_name = azurerm_resource_group.example.name
   loadbalancer_id     = azurerm_lb.lb.id
   name                = "nw-backend"
 }
